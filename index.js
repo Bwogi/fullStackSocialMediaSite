f859bc94d593a33109bcfe2ac85a992341a9c937
Andrew Bwogi <Andrew.bwogi@gmail.com>
2021-10-23 11:51:01 -0700

    graphQL server connected to MONGODB-check /Users/ucberkeley/Desktop/MERN-G/mySocialMediaApp on fillstackweb account

---
+++ index.js
@@ -0,0 +1,33 @@
+// set apollo server to start running 
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config.js');
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World' 
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MONGODB connected...')
+    return server.listen({ port:5000 })
+    })
+    .then((res) => {
+    console.log(`🚀  Server ready at ${res.url}`);
+  });
